c2199eb6b713216389ea8e9523bb830f17c01eb0
Josh Walsh <jwalsh@designinginteractive.com>
2010-01-31 22:10:02 -0500

    refactoring

--- numbers.rb
+++ numbers.rb
@@ -1,9 +1,5 @@
 class Numbers
   def check_entry(entered_sequence)
-    if entered_sequence == '4 8 15 16 23 42'
-      true
-    else
-      false
-    end
+    entered_sequence == '4 8 15 16 23 42'
   end
 end
