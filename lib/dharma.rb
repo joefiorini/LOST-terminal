8f9d96b9b2e039d99d9bb15ee4fcba0f58295091
Joe Fiorini <joe@faithfulgeek.org>
2010-02-01 22:12:14 -0500

    More shell code for dharma class

--- lib/dharma.rb
+++ lib/dharma.rb
@@ -1,13 +1,38 @@
 require 'highline'
+require File.dirname(__FILE__) + '/numbers'
 
 class Dharma
 
   def initialize(terminal, &block)
     @terminal = terminal
+    @numbers = Numbers.new
     self.instance_eval(&block)
   end
 
   def volunteer_for_program
     StoryPrinter.print(:volunteer_for_program)
+  end
+
+  def ask_for_numbers
+    @numbers.set(@terminal.input.gets)
+  end
+
+  def plane_crashed!
+    StoryPrinter.print(:plane_crashed, @terminal.output)
+  end
+
+  def plane_crashed?
+    @numbers.valid?
+  end
+
+  def appeal_to_smoke_monster!
+    StoryPrinter.print(:appeal_to_smoke_moster)
+    generate_drawing_entry!
+  end
+
+  def generate_drawing_entry!
+    StoryPrinter.print(:success)
+    # generate number
+    # write to file
   end
 end
