a336a80168fc895156bc72add936e0fd90cc6925
Joe Fiorini <joe@faithfulgeek.org>
2010-02-03 18:19:43 -0500

    Add sound files to project

--- lib/sound_player.rb
+++ lib/sound_player.rb
@@ -1,15 +1,16 @@
 class SoundPlayer
   attr_reader :sounds
+  @@assets_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", "assets"))
 
   def initialize
-    @sounds = {:alarm => "~/Desktop/timer_alarm.mp3",
-              :reset => "~/Desktop/timer_reset.mp3",
-              :magnet => "~/Desktop/em_metal_objects.mp3",
-              :system_failure => "~/Desktop/system_failure_triple.mp3"}
+    @sounds = {:alarm => path_to("timer_alarm.mp3"),
+              :reset => path_to("timer_reset.mp3"),
+              :magnet => path_to("em_metal_objects.mp3"),
+              :system_failure => path_to("system_failure_triple.mp3")}
   end
 
   def play_sound(sound)
-    `mpg123 #{sounds[sound]} 2> /dev/null`
+    `/opt/local/bin/mpg123 #{sounds[sound]} 2> /dev/null`
   end
 
   def play_sound_looped(sound)
@@ -23,5 +24,9 @@ class SoundPlayer
 
   def stop_sound!
     Thread.kill(@sound_thread)
+  end
+  
+  def path_to(filename)
+    "%s/%s" % [@@assets_dir, filename]
   end
 end
